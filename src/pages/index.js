244874380a25303f93d341e01e50c16a241e1312
Frannsoft <Frannsoftdev@outlook.com>
2020-11-18 08:13:37 -0500

    initial checkin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,33 @@
-import React from "react"
+import React from "react";
+import { graphql } from "gatsby";
+import BackgroundImage from "gatsby-background-image";
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default ({ data }) => {
+
+  const backgroundImage = data.file.childImageSharp.fluid;
+
+  const backgroundImageFluidStack = [
+    `linear-gradient(rgba(255,255,255,0.85), rgba(255,255,255,0.85))`,
+    backgroundImage,
+  ];
+
+  return (<BackgroundImage Tag="div" fluid={backgroundImageFluidStack}>
+    <div style={{ height: `700px` }}>
+      <span>Hello World!</span>
+    </div>
+  </BackgroundImage>);
+};
+
+export const query = graphql`
+query {
+  file(relativePath: { eq: "road(1).jpg" }) {
+    childImageSharp {
+      # Specify the image processing specifications right in the query.
+      # Makes it trivial to update as your page's design changes.
+      fluid(maxWidth:2000) {
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    }
+  }
 }
+`;
